1b50215d761b2abdca971682502a39950f4e23f2
Meredith Coyne <meredithleigh.coyne@gmail.com>
2021-11-16 11:43:34 -0500

    updated listener to server.js

--- server.js
+++ server.js
@@ -12,7 +12,21 @@ app.use(express.json());
 app.use(express.static("public"));
 
 // add routes
+app.use(require("./routes/html-routes.js"));
+app.use(require("./routes/api-routes.js"));
 
 // connect to db
+mongoose.connect(
+    process.env.MONGODB_URI || "mongodb://localhost/workout",
+    {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useCreateIndex: true,
+        useFindAndModify: false
+    }
+);
 
 // start server
+app.listen(PORT, () => {
+    console.log(`App running on port ${PORT}!`);
+});
