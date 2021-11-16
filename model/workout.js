beb1f686205e29ed361d9c9912560166d142c0e9
Meredith Coyne <meredithleigh.coyne@gmail.com>
2021-11-16 11:30:58 -0500

    fixed erros in workout js file

--- model/workout.js
+++ model/workout.js
@@ -1,62 +1,62 @@
-// depencies for using mongoose
+// dependencies for using mongoose
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 // workout model
-const workoutSchema = new Schema({
-        day: {
-            type: Date,
-            default: Date.now,
+const WorkoutSchema = new Schema({
+    day: {
+        type: Date,
+        default: Date.now,
+    },
+    exercises: [{
+
+        type: {
+            type: String,
+            trim: true,
+            required: "Please enter the type of exercise."
         },
 
-        exercises: [{
-            type: {
-                type: String,
-                trim: true,
-                required: "Please enter the type of exercise."
-            },
+        name: {
+            type: String,
+            trim: true,
+            required: "Please enter the name of your exercise."
+        },
 
-            name: {
-                type: String,
-                trim: true,
-                required: "Please enter the name of your exercise."
-            },
+        duration: {
+            type: Number,
+            trim: true,
+            required: "Please enter the duration you completed for our exercise."
+        },
 
-            duration {
-                type: Number,
-                trim: true,
-                required: "Please enter the duration you completed for our exercise."
-            },
+        weight: {
+            type: Number,
+            trim: true,
+            required: "Please enter the weight you used for your exercise."
+        },
 
-            weight: {
-                type: Number,
-                trim: true,
-                required: "Please enter the weight you used for your exercise."
-            },
+        reps: {
+            type: Number,
+            trim: true,
+            required: "Please enter how many reps you completed for your exercise."
+        },
 
-            reps: {
-                type: Number,
-                trim: true,
-                required: "Please enter how many reps you completed for your exercise."
-            },
+        sets: {
+            type: Number,
+            trim: true,
+            required: "Please enter how many sets your completed for your exercise."
+        },
 
-            sets: {
-                type: Number,
-                trim: true,
-                required: "Please enter how many sets your completed for your exercise."
-            },
-
-            distance: {
-                type: Number,
-                trim: true,
-                required: "Please enter the distance you completed for your exercise."
-            }
+        distance: {
+            type: Number,
+            trim: true,
+            required: "Please enter the distance you completed for your exercise."
         }
-    ],
-},
+    }],
+}, 
+
 {
-        toJSON: {
-            virtuals: true
-        }
+    toJSON: {
+        virtual: true
     }
+}
 );
