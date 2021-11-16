aded89da12d5b2d0cd873ce1fe762945c5f0065f
Meredith Coyne <meredithleigh.coyne@gmail.com>
2021-11-16 18:13:38 -0500

    updated POST routes

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -29,11 +29,23 @@ router.post("/api/workouts/:id", ({body, params}, res) => {
 
 // POST route
 router.get("/api/workouts", ({body}, res) => {
-
+    db.Workout.create({body})
+    .then(dbWorkout => {
+        res.json(dbWorkout)
+    })
+    .catch(err => {
+        res.json(err);
+    });
 });
 
 router.get("/api/workouts/range", (req, res) => {
-
+    db.Workout.find({}).limit(5)
+    .then(dbWorkout => {
+        res.json(dbWorkout)
+    })
+    .catch(err => {
+        res.json(err);
+    });
 });
 
 module.exports = router;
