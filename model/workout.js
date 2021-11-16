53d238025c9f6e855e2350937ad9666749d285e3
Meredith Coyne <meredithleigh.coyne@gmail.com>
2021-11-16 11:23:59 -0500

    updated workout js file with array for exercise steps

--- model/workout.js
+++ model/workout.js
@@ -4,59 +4,57 @@ const Schema = mongoose.Schema;
 
 // workout model
 const workoutSchema = new Schema({
-            day: {
-                type: Date,
-                default: Date.now,
+        day: {
+            type: Date,
+            default: Date.now,
+        },
+
+        exercises: [{
+            type: {
+                type: String,
+                trim: true,
+                required: "Please enter the type of exercise."
             },
 
-            excersise: [{
-                type: {
-                    type: ,
-                    trim: ,
-                    required: ""
-                },
+            name: {
+                type: String,
+                trim: true,
+                required: "Please enter the name of your exercise."
+            },
 
-                name: {
-                    type: ,
-                    trim: ,
-                    required: ""
-                },
+            duration {
+                type: Number,
+                trim: true,
+                required: "Please enter the duration you completed for our exercise."
+            },
 
-                duration {
-                    type: ,
-                    trim: ,
-                    required: ""
-                },
+            weight: {
+                type: Number,
+                trim: true,
+                required: "Please enter the weight you used for your exercise."
+            },
 
-                weight: {
-                    type: ,
-                    trim: ,
-                    required: ""
-                },
+            reps: {
+                type: Number,
+                trim: true,
+                required: "Please enter how many reps you completed for your exercise."
+            },
 
-                reps: {
-                    type: ,
-                    trim: ,
-                    required: ""
-                },
+            sets: {
+                type: Number,
+                trim: true,
+                required: "Please enter how many sets your completed for your exercise."
+            },
 
-                sets: {
-                    type: ,
-                    trim: ,
-                    required: ""
-                },
-
-                distance: {
-                    type: ,
-                    trim: ,
-                    required: ""
-                }
-
-            }],
-        },
-    },
-
-    {
+            distance: {
+                type: Number,
+                trim: true,
+                required: "Please enter the distance you completed for your exercise."
+            }
+        }
+    ],
+},
+{
         toJSON: {
             virtuals: true
         }
